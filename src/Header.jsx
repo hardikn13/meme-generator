86ad94ac9c84d139cabcf2e867978ac84bedf118
Hardik <hardiknahta111@gmail.com>
2023-07-30 17:20:21 +0530

    meme-generator created

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="header">
+            <img className="header--image" src="images/image (1).png"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">Using ReactJS</h4>
+        </div>
+    )
+}
+
+export default Header
